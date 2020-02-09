972d04afa6411d89d8d25cddc203e78a36d45049
ShahriarAsfaq <shahriarutsha@gmail.com>
2020-02-10 02:19:04 +0600

    first commit

--- app/src/main/java/com/example/iutguide/HomePage.java
+++ app/src/main/java/com/example/iutguide/HomePage.java
@@ -2,13 +2,32 @@ package com.example.iutguide;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class HomePage extends AppCompatActivity {
-
+    private Button teacher;
+    private Button student;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
+        teacher=(Button)findViewById(R.id.teacher);
+        student=(Button)findViewById(R.id.student);
+        teacher.setOnClickListener(new View.OnClickListener() {
+                                       @Override
+                                       public void onClick(View v) {
+                                           Intent intent = new Intent(HomePage.this, LogIn.class);
+                                           startActivity(intent);
+                                       }
+                                   }
+            student.setOnClickListener(new View.OnClickListener() {
+                @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(HomePage.this,LogIn.class);
+                startActivity(intent);
     }
 }
+    }
