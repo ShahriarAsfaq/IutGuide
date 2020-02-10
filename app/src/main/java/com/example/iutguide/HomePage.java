08b7c8e6fa8b6d1f336f39976e7c99cfe19442d0
ShahriarAsfaq <shahriarutsha@gmail.com>
2020-02-10 13:30:09 +0600

    q

--- app/src/main/java/com/example/iutguide/HomePage.java
+++ app/src/main/java/com/example/iutguide/HomePage.java
@@ -10,24 +10,26 @@ import android.widget.Button;
 public class HomePage extends AppCompatActivity {
     private Button teacher;
     private Button student;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
-        teacher=(Button)findViewById(R.id.teacher);
-        student=(Button)findViewById(R.id.student);
+        teacher = (Button) findViewById(R.id.teacher);
+        student = (Button) findViewById(R.id.student);
         teacher.setOnClickListener(new View.OnClickListener() {
-                                       @Override
-                                       public void onClick(View v) {
-                                           Intent intent = new Intent(HomePage.this, LogIn.class);
-                                           startActivity(intent);
-                                       }
-                                   }
-            student.setOnClickListener(new View.OnClickListener() {
-                @Override
+            @Override
             public void onClick(View v) {
                 Intent intent=new Intent(HomePage.this,LogIn.class);
                 startActivity(intent);
+            }
+        });
+        student.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(HomePage.this,StudentAccount.class);
+                startActivity(intent);
+            }
+        });
     }
 }
-    }
