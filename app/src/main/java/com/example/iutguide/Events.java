e794ba97aca7fc561ecf015b30f8b5a45c8bf6ab
ShahriarAsfaq <shahriarutsha@gmail.com>
2020-03-10 20:42:16 +0600

    event database

--- app/src/main/java/com/example/iutguide/Events.java
+++ app/src/main/java/com/example/iutguide/Events.java
@@ -7,16 +7,23 @@ import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.List;
+
 public class Events extends AppCompatActivity {
     Intent intent= getIntent();
     ListView eventlist;
+    DatabaseReference event_List;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_events);
         eventlist=(ListView)findViewById(R.id.eventlist);
-        String[] event_List= getResources().getStringArray(R.array.event_list);
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(Events.this,R.layout.sample_layout,R.id.listText,event_List);
+        //databaseReference= FirebaseDatabase.getInstance().getReference("Event1");
+        event_List= FirebaseDatabase.getInstance().getReference("Event1");
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(Events.this,R.layout.sample_layout,R.id.listText, (List<String>) event_List);
         eventlist.setAdapter(adapter);
     }
 }
